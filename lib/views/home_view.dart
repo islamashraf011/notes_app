fdad521cd6b7b0c7c37b6c35efb543edefd392c5
I S L A M <islamashraf1539@gmail.com>
2023-12-19 02:16:12 +0200

    Create Bloc Provider to Notes Cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notesapp/widgets/notes_view_body.dart';
 
 import '../widgets/custom_floating_action_button.dart';
@@ -8,9 +10,12 @@ class HomeView extends StatelessWidget {
   static String id = "HomeView";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      floatingActionButton: CustomFloatingActionButton(),
-      body: NotesViewBody(),
+    return BlocProvider<NotesCubit>(
+      create: (context) => NotesCubit(),
+      child: const Scaffold(
+        floatingActionButton: CustomFloatingActionButton(),
+        body: NotesViewBody(),
+      ),
     );
   }
 }
