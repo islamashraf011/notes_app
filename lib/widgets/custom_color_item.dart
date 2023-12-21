887fb80f133d375e0fdff42576231399f487a808
I S L A M <islamashraf1539@gmail.com>
2023-12-21 03:28:16 +0200

    Make Color Item can Picked then change his shape

--- lib/widgets/custom_color_item.dart
+++ lib/widgets/custom_color_item.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.isActive, this.onTap});
+  final bool isActive;
+  final Function()? onTap;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(right: 15),
-      child: CircleAvatar(
-        backgroundColor: Colors.white,
-        radius: 36,
-        child: CircleAvatar(
-          backgroundColor: Colors.red,
-          radius: 33,
-        ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 15),
+        child: isActive
+            ? const CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 37,
+                child: CircleAvatar(
+                  backgroundColor: Colors.red,
+                  radius: 32,
+                ),
+              )
+            : const CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 33,
+              ),
       ),
     );
   }
--- lib/widgets/custom_color_list_view.dart
+++ lib/widgets/custom_color_list_view.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-
 import 'custom_color_item.dart';
 
-class ColorItemListView extends StatelessWidget {
+class ColorItemListView extends StatefulWidget {
   const ColorItemListView({super.key});
+
+  @override
+  State<ColorItemListView> createState() => _ColorItemListViewState();
+}
+
+class _ColorItemListViewState extends State<ColorItemListView> {
+  int curentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +19,13 @@ class ColorItemListView extends StatelessWidget {
         itemCount: 10,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return ColorItem(
+            isActive: curentIndex == index,
+            onTap: () {
+              curentIndex = index;
+              setState(() {});
+            },
+          );
         },
       ),
     );
